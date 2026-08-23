db90464c6adff2e1f806ff0b0c8c1cc02899dc7a
ALLWIN INFANT <allwininfant2006@gmail.com>
2026-08-23 17:30:45 +0530

    Add files via upload

---
+++ app.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner ip=new Scanner (System.in);
+        int num=ip.nextInt();
+if(num%2==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("Odd");
+}
+}
+}
